5bd080662b40dc08bc9d49e23bfbb5b184882404
vidakomaria <vidakomaria47@gmail.com>
2021-06-10 23:52:19 +0700

    Update transaksi_panel.py

--- transaksi_panel.py
+++ transaksi_panel.py
@@ -6,3 +6,22 @@ class panelTransaksi (wxFrame.wxTransaksiPanel):
     def __init__(self, parent):
         wxFrame.wxTransaksiPanel.__init__(self, parent)
         self.conn = database.Database()
+
+    def btnTmbhTransOnButtonClick( self, event ):
+        nama = self.textCtrlNamaBrgTrans.GetValue()
+        kuantitas = self.textCtrlKuantitasTrans.GetValue()
+        hrg = self.textCtrlHrgTrans.GetValue()
+        value = (nama, kuantitas, hrg)
+        query = self.conn.set_query ("INSERT INTO `transaksi`(`nama barang`, `kuantitas`, `harga`) VALUES (%s,%s,%s)",value)
+
+        if (query.commit().get_rowcount() > 0):
+            wx.MessageBox("Data Transaksi berhasil ditambahkan")
+            self.clearText()
+
+        else:
+            wx.MessageBox("Data Transaksi gagal Ditambah", "Gagal" | wx.ICON_ERROR)
+
+    def clearText(self):
+        self.textCtrlNamaBrgTrans.SetValue('')
+        self.textCtrlHrgTrans.SetValue('')
+        self.textCtrlKuantitasTrans.SetValue('')
