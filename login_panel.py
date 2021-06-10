01678aa51445c242d8f7e9ab88a2f9f25073d7dd
vidakomaria <vidakomaria47@gmail.com>
2021-06-10 22:34:33 +0700

    Update login_panel.py

--- login_panel.py
+++ login_panel.py
@@ -1,22 +1,27 @@
 import wx
-import WX.wx as frame
-import database.database as database
+import wxFrame
+import database
+import panel
 
-class PanelLogin (frame.wxLoginPanel):
+class panelLogin (wxFrame.wxLoginPanel):
     def __init__(self, parent):
-        frame.wxLoginPanel.__init__(self, parent)
+        wxFrame.wxLoginPanel.__init__(self, parent)
         self.conn = database.Database()
 
     def btnLoginOnButtonClick(self, event):
         idPegawai = self.textCtrlIdPeg.GetValue()
         password = self.textCtrlPassword.GetValue()
-        nama = "kasir1"
         query = self.conn.set_query("SELECT * FROM admin WHERE id_pegawai = '%s'" % (idPegawai))
         result = query.fetchone()
         if (result is not None):
             print(result)
             if result[3]== password:
                 wx.MessageBox("Login Berhasil")
-                print("login")
+                self.textCtrlPassword.SetValue('')
+                panel.Init.login.Hide()
+                panel.Init.transaksi.Show()
             else:
                 self.textCtrlPassword.SetValue('')
+                wx.MessageBox("Password Salah", "Warning", wx.OK | wx.ICON_ERROR)
+        else:
+            wx.MessageBox("ID Pegawai tidak ditemukan", "Warning" , wx.OK| wx.ICON_ERROR)
