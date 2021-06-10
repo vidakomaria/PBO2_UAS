668fcbceeeb6998e3b8f18cf8c0f0dfecfa57a96
vidakomaria <vidakomaria47@gmail.com>
2021-06-10 21:31:58 +0700

    Update connection_try.py

--- database/connection_try.py
+++ database/connection_try.py
@@ -2,11 +2,7 @@ import mysql.connector
 from mysql.connector import Error
 
 try:
-    connection = mysql.connector\
-                      .connect(host='localhost',
-                               database='data_baju',
-                               user='root',
-                               password='')
+    connection = mysql.connector.connect(host='localhost', database='data_baju', user='root', password='')
     if connection.is_connected():
         db_info = connection.get_server_info()
         print("Berhasil Terhubung deng core ", db_info)
@@ -16,8 +12,9 @@ try:
         print("You're connected to db: ", record)
 except Error as e:
     print("Koneksi Error : tidak bisa terkoneksi dengan core mysql ", e)
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("Koneksi core ditutup")
+# finally:
+#     if connection.is_connected():
+#         cursor.close()
+#         connection.close()
+#         print("Koneksi core ditutup")
+
